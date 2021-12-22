c95042ed2485aa8910c38585a7d9dd6a8d9a11c2
Christian Valenti <christian.valenti@uni-ulm.de>
2021-12-22 21:35:48 +0100

    Started chapter 7 :book: :clock930:

---
+++ c/chapter07.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// exercise 1
+void one() {
+    
+}
+
+void two() {}
+
+void three() {}
+
+void four() {}
+
+void five() {}
+
+void six() {}
+
+void seven() {}
+
+void eight() {}
+
+void nine() {}
+
+void ten() {}
+
+int main() {
+    int exercise;
+    printf("Choose the exercise to do (1-10).\n");
+    scanf("%d", &exercise);
+
+    if (exercise == 1) {
+        one();
+    } else if (exercise == 2) {
+        two();
+    } else if (exercise == 3) {
+        three();
+    } else if (exercise == 4) {
+        four();    
+    } else if (exercise == 5) {
+        five();
+    } else if (exercise == 6) {
+        six();
+    } else if (exercise == 7) {
+        seven();
+    } else if (exercise == 8) {
+        eight();
+    } else if (exercise == 9) {
+        nine();
+    } else if (exercise == 10) {
+        ten();
+    }
+}
